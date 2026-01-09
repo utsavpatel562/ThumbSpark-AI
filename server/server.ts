14db5645dd3e3eed0fdd9db2e994e0f3ea959eff
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2026-01-08 20:47:34 -0500

    latest commit

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,11 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-const port = 3000;
-
+app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
