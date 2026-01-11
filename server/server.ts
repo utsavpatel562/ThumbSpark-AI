97046bb74ec83d997300c5cfebcc2c6f656f8a3b
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2026-01-11 11:03:44 -0500

    latest commit

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
-
+import cors from 'cors';
 const app = express();
-
+app.use(cors());
 app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
